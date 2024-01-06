f032819a587cafa49189b1571a668336087782ea
Muhammed Ziyad <muhdziyadu@gmail.com>
2024-01-06 16:43:14 +0530

    ticket page route set

--- src/routes/studentRoute.jsx
+++ src/routes/studentRoute.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
 import Register from '../pages/student/register'
 import StudentLogin from '../pages/student/studentLogin'
+import TicketsPage from '../pages/student/ticketsPage'
+import { useSelector } from 'react-redux'
 
 function StudentRoute() {
+
+    const {token} = useSelector(state=>state.Student)
     return (
         <div>
             <Routes>
                 <Route path='/register' element={<Register/>} />
                 <Route path='/login' element={<StudentLogin/>} />
+                <Route path='/' element={token?<TicketsPage/>:<Navigate to={'/login'}/>} />
                 {/* <Route path='/*' element={<NotFound />} /> */}
             </Routes>
         </div>
