3dbee2791cb6275723ffe9818e902cb3f3bdf122
Muhammed Ziyad <muhdziyadu@gmail.com>
2024-01-09 12:50:43 +0530

    env set on frontend

--- src/components/ticketListTable.jsx
+++ src/components/ticketListTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function TicketListTable({ tickets, setTicketDetails, setTicket, search }) {
-
+    
     return (
         <div className="relative overflow-x-auto md:mx-5 sm:mx-3 mx-1">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -37,7 +37,7 @@ function TicketListTable({ tickets, setTicketDetails, setTicket, search }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {tickets.filter((ticket => (ticket?.subject ? ticket?.ticketSubject?.subject?.toLowerCase().includes(search) : ticket))).map(ticket => {
+                    {tickets?.filter((ticket => (ticket?.subject ? ticket?.ticketSubject?.subject?.toLowerCase().includes(search) : ticket))).map(ticket => {
                         return (
                             <tr key={ticket?.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
