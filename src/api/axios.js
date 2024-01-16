c7dd546faaadc3bc3e12b471c96915fa15a33bef
Muhammed Ziyad <muhdziyadu@gmail.com>
2024-01-16 21:58:03 +0530

    created axios instance for admin and student

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
     baseURL:import.meta.env.VITE_SERVERURL,
     headers:{
-        "Content-Type":'application/json'
+        'Content-Type':'application/json'
     }
 })
  
---
+++ src/api/studentAxios.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { useSelector } from 'react-redux'
+
+const axiosInstance = axios.create({
+    baseURL:import.meta.env.VITE_SERVERURL
+})
+
+axiosInstance.interceptors.request.use(config=>{
+    const {token} = useSelector(store=>store.Student)
+    config.headers.Authorization = token ?  `Bearer ${token}` : null
+})
+ 
+export default axiosInstance
